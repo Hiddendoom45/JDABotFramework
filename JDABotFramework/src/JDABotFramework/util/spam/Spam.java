a41e86967c8f300ba4d80d15f7a92f7a1407e64f
Allen <chonglun.chen@gmail.com>
2018-08-03 12:02:44 -0400

    Spam stuff
    -things to control/limit spam stuffs

---
+++ JDABotFramework/src/JDABotFramework/util/spam/Spam.java
@@ -0,0 +1,96 @@
+package JDABotFramework.util.spam;
+
+
+import java.util.HashMap;
+
+/**
+ * Item representing frequency of user action
+ * Each one represents user activity in each of the areas/types
+ * Executed to determine if user has been spamming specific bot commands that are spam limited
+ * @author Allen
+ *
+ */
+public class Spam {
+	private HashMap<String,SpamItem[]> items=new HashMap<String,SpamItem[]>();
+	
+	/**
+	 * Checks if something is spam
+	 * @param type type used to differentiate between things like modules etc
+	 * @return if there's been enough requests in 5 min for it to constitute as spam
+	 */
+	public SpamData isSpam(String type){
+		return isSpam(type,5,120000);
+	}
+	public SpamData isSpam(String type,int defaultSize){
+		return isSpam(type,defaultSize,120000);
+	}
+	public SpamData isSpam(String type, int defaultSize,int timeout){
+		boolean trigger=false;//if it's not spam trigger to true
+		long tillDone=Long.MAX_VALUE;
+		if(items.containsKey(type)){//if type is recorded
+			boolean recorded=false;//if current time has been recorded to a slot
+			SpamItem[] spams=items.get(type);//get times it was used
+			for(int i=0;i<spams.length;i++){//iterate
+				if(!(spams[i]==null)){//avoid null case, nothing recorded in that spot
+					if(spams[i].timeout()){//if it's timed out, change it back to null, free up spot
+						if(recorded){
+							spams[i]=null;//change to null if current spam item has been recorded
+							trigger=true;
+						}
+						else{//else record it
+							spams[i]=new SpamItem(timeout);
+							recorded=true;
+							trigger=true;
+						}
+					}
+					if(!(spams[i]==null)&&tillDone>spams[i].tillDone()){
+						tillDone=spams[i].tillDone();
+					}
+				}
+				else{
+					trigger=true;
+					if(!recorded){//if not recorded, record to a null slot
+						spams[i]=new SpamItem(timeout);
+						recorded=true;
+					}
+				}
+			}
+			items.put(type, spams);
+		}
+		else{
+			SpamItem[] spams=new SpamItem[defaultSize];
+			spams[0]=new SpamItem(timeout);
+			items.put(type, spams);
+			trigger=true;
+		}
+		return new SpamData(trigger,tillDone);
+	}
+	
+	
+	private class SpamItem{
+		private long time;
+		private long timeout;
+		public SpamItem(int timeout){
+			time=System.currentTimeMillis();
+			this.timeout=timeout;
+		}
+		public long tillDone(){
+			return (time+timeout)-System.currentTimeMillis();
+		}
+		public boolean timeout(){
+			if(System.currentTimeMillis()>(time+timeout))return true;
+			return false;
+		}
+
+	}
+	public class SpamData{
+		public boolean isSpam;
+		public long tillDone;
+		SpamData(boolean isSpam, long tillDone){
+			this.isSpam=isSpam;
+			this.tillDone=tillDone;
+		}
+	}
+	
+	
+}
---
+++ JDABotFramework/src/JDABotFramework/util/spam/SpamControl.java
@@ -0,0 +1,62 @@
+package JDABotFramework.util.spam;
+
+import java.util.HashMap;
+
+import JDABotFramework.util.spam.Spam.SpamData;
+import JDABotFramework.wrapper.JDAMessage;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+/**
+ * Class used to control spam, calls to isSpam will determine if user has been spaming 'type'too much
+ * @author Allen
+ *
+ */
+public class SpamControl {
+	private static HashMap<String,Spam> spammers=new HashMap<String,Spam>();
+	private static HashMap<String,int[]> typeData=new HashMap<String,int[]>();//hashmap with array containing data on timeout and number of spaces for spam control
+	private static HashMap<MessageReceivedEvent, Long> spamData=new HashMap<MessageReceivedEvent,Long>();//times and when spam messages have been sent
+	private static final int spamPersistance=20;
+	public static boolean isSpam(MessageReceivedEvent event,String type){
+		String key=getTypeData(type)[0]==0?key1(event):key2(event);//determine if it's global or channel locked spam controlling
+		int defaultSize=getTypeData(type)[1];//size of max entries before trigger
+		int timeout=getTypeData(type)[2];//timeout for each entry to exit controller
+		if(spammers.containsKey(key)){//if tracked in the current instance
+			JDABotFramework.util.spam.Spam.SpamData spam= spammers.get(key).isSpam(type,defaultSize,timeout);
+			if(!spam.isSpam){
+				sendSpamMessage(event,type,spam);
+			}
+			return spam.isSpam;
+		}
+		else{
+			Spam spammer=new Spam();
+			SpamData spam=spammer.isSpam(type,defaultSize,timeout);
+			spammers.put(key, spammer);
+			if(!spam.isSpam){
+				sendSpamMessage(event,type,spam);
+			}
+			return spam.isSpam;
+		}
+	}
+	public static void setSpams(){
+		//type(0==local,1==global),limit, timeout
+		typeData.put("summon", new int[]{1,2,60000});
+		typeData.put("units", new int[]{0,4,60000});
+	}
+	private static void sendSpamMessage(MessageReceivedEvent event,String type,SpamData spam){
+		if(!spamData.containsKey(event)){
+			spamData.put(event, System.currentTimeMillis());
+			JDAMessage.sendTempMessage(event, "**%userName%** too many messages, please wait **"+(spam.tillDone/1000)+"** seconds", spamPersistance);
+		}
+	}
+	private static String key1(MessageReceivedEvent event){
+		return event.getAuthor().getId()+event.getChannel().getId();//based off of person and channel, as spam is going to be set in part on an event basis
+	}
+	private static String key2(MessageReceivedEvent event){
+		return event.getAuthor().getId();//based off of person a universal spam control, for certain commands(aka summon simulator)
+	}
+	private static int[] getTypeData(String type){
+		return typeData.containsKey(type)?typeData.get(type):new int[]{1,5,120000};//default uses global control, limit of 5 every 2 min
+	}
+	
+	
+}
