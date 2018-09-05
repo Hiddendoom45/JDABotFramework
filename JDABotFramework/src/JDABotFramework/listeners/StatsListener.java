47cc1e5b2cc0be35b4a44276a0b4f235fcafb423
Allen <chonglun.chen@gmail.com>
2018-09-05 14:32:07 -0400

    Template for a listener that collects statistics

---
+++ JDABotFramework/src/JDABotFramework/listeners/StatsListener.java
@@ -0,0 +1,11 @@
+package JDABotFramework.listeners;
+
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+/**
+ * Listener that records the stats of each event
+ * @author Allen
+ *
+ */
+public class StatsListener extends ListenerAdapter{
+	
+}
