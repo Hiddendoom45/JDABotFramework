c3db637b739213904af480fb1a7442a00118fb7a
Allen <chonglun.chen@gmail.com>
2018-11-09 13:45:12 -0500

    Error throwing when incorrect state
    -when saving/loading if storage source not set
    -hopefully avoid some coding errors

--- JDABotFramework/src/JDABotFramework/global/config/guild/GuildConfigController.java
+++ JDABotFramework/src/JDABotFramework/global/config/guild/GuildConfigController.java
@@ -1,5 +1,7 @@
 package JDABotFramework.global.config.guild;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.HashMap;
 
 import com.google.gson.Gson;
@@ -19,7 +21,11 @@ public class GuildConfigController {
 	public GuildConfigController(BotGlobalConfig gConfig){
 		this.gConfig=gConfig;
 	}
-	
+	/**
+	 * Sets the storage source to be used when saving/loading without storage source arty.
+	 * Silently ignores null.
+	 * @param store
+	 */
 	public void setStorageSource(KeyStorageInt store){
 		if(!(store==null)){
 			this.store=store;
@@ -28,10 +34,14 @@ public class GuildConfigController {
 	/**
 	 * Load values from storage source
 	 * storage source needs to be set using {@link #setStorageSource(KeyStorageInt)}
+	 * @throws UncheckedIOException if storage source not set using {@link #setStorageSource(KeyStorageInt)}
 	 */
 	public void load(){
 		if(!(store==null)){
 			load(store);
+		}
+		else{
+			throw new UncheckedIOException(new IOException("load:Storage source not set"));
 		}
 	}
 	/**
@@ -61,10 +71,14 @@ public class GuildConfigController {
 	/**
 	 * update hook to update a guild whenever something about it changes
 	 * @param id guildID
+	 * @throws UncheckedIOException if storage source not set using {@link #setStorageSource(KeyStorageInt)}
 	 */
 	public void update(String id){
 		if(!(store==null)){
 			update(id,store);
+		}
+		else{
+			throw new UncheckedIOException(new IOException("update:Storage source not set"));
 		}
 	}
 	/**
@@ -78,10 +92,14 @@ public class GuildConfigController {
 	/**
 	 * Only update guild index, nothing else
 	 * storage source needs to be set using {@link #setStorageSource(KeyStorageInt)}
+	 * @throws UncheckedIOException if storage source not set using {@link #setStorageSource(KeyStorageInt)}
 	 */
 	public void quickSave(){
 		if(!(store==null)){
 			quickSave(store);
+		}
+		else{
+			throw new UncheckedIOException(new IOException("quicksave:Storage source not set"));
 		}
 	}
 	/**
@@ -95,11 +113,15 @@ public class GuildConfigController {
 	}
 	/**
 	 * Fully update everything pushing to the storage source.
-	 * storage source needs to be set using {@link #setStorageSource(KeyStorageInt)} 
+	 * storage source needs to be set using {@link #setStorageSource(KeyStorageInt)}
+	 * @throws UncheckedIOException if storage source not set using {@link #setStorageSource(KeyStorageInt)} 
 	 */
 	public void save(){
 		if(!(store==null)){
 			save(store);
+		}
+		else{
+			throw new UncheckedIOException(new IOException("save:Storgae source not set"));
 		}
 	}
 	/**
